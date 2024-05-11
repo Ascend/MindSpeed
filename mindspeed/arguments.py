d641ffa7f550506661791478782a409e42da6354
wangyunhang <wangyunhang@huawei.com>
2024-05-11 07:20:13 +0000

    !279 自适应选择性重计算UT适配最新PTA，自适应重计算和全重计算同时打开后关闭全重计算 Merge pull request !279 from wangyunhang/1.0

--- mindspeed/arguments.py
+++ mindspeed/arguments.py
@@ -184,6 +184,12 @@ def validate_args_decorator(validate_args):
         if args.tp_comm_overlap:
             args.tp_comm_overlap = False
 
+        adaptive_recompute_enable = args.adaptive_recompute_device_size > 0 or args.adaptive_recompute_device_swap
+        if adaptive_recompute_enable and (args.recompute_granularity is not None or args.recompute_method is not None):
+            print("[WARNING] if activate adaptive selective recompute, recomputing args switch "
+                        "\"recompute_granularity\" and \"recompute_method\" will be turned off.")
+            args.recompute_granularity = None
+            args.recompute_method = None
     return wrapper
 
 
