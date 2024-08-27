370b983dc11a2602e24df0b9b61bdc58b6268528
李宝奎 <libaokui@huawei.com>
2024-08-27 01:22:47 +0000

    !703 修复日志多打印问题 Merge pull request !703 from 李宝奎/core060_rizhi

--- mindspeed/megatron_adaptor.py
+++ mindspeed/megatron_adaptor.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 from functools import wraps
 import torch
+from torch.distributed import all_gather_into_tensor, reduce_scatter_tensor
 from torch_npu.contrib import transfer_to_npu
 from .arguments import process_args
 
@@ -147,6 +148,8 @@ def torch_adaptation(aspm):
     aspm.register_patch('torch.Tensor.type', type_wrapper)
     aspm.register_patch('torch.Tensor.view', ensure_contiguous_wrapper)
     aspm.register_patch('torch.Tensor.repeat_interleave', repeat_interleave)
+    aspm.register_patch('torch.distributed._all_gather_base', all_gather_into_tensor)
+    aspm.register_patch('torch.distributed._reduce_scatter_base', reduce_scatter_tensor)
 
 
 def megatron_core_adaptation(aspm, mindspeed_args):
