9ab763ae39d3482dbad7fead736f9f4b1b7d2a66
邓佳 <dengjia6@huawei.com>
2024-10-21 09:33:45 +0000

    !1086 fix: npu_ring_attention_update when actual_seq_qlen is none; perf: tests Merge pull request !1086 from 邓佳/1.0.RC3_core_r0.6.0_fix

--- tests_extend/unit_tests/ops/cann/test_npu_fused_moe_token_permute_and_unpermute.py
+++ tests_extend/unit_tests/ops/cann/test_npu_fused_moe_token_permute_and_unpermute.py
@@ -5,108 +5,113 @@ import torch
 import torch_npu
 
 from mindspeed import megatron_adaptor
-from unit_tests.common import DistributedTest
-from megatron.training.arguments import parse_args
-from megatron.training.global_vars import set_args
 from megatron.core.transformer.moe.moe_utils import permute
 from megatron.core.transformer.moe.moe_utils import unpermute
+from mindspeed.core.fusions.npu_moe_token_permute import permute_wrapper
+from mindspeed.core.fusions.npu_moe_token_unpermute import unpermute_wrapper
+
+from unit_tests.common import TOL_MAPPING
+
+fused_permute = permute_wrapper(permute)
+fused_unpermute = unpermute_wrapper(unpermute)
 
 
-def create_test_args(use_fused_moe_token_permute_and_unpermute=False):
-    args = parse_args(None, True)
-    args.use_fused_moe_token_permute_and_unpermute = use_fused_moe_token_permute_and_unpermute
-    return args
-
-
-class TestNpuFusedPermuteAndUnpermute(DistributedTest):
-    world_size = 1
+class TestNpuFusedPermuteAndUnpermute():
 
     @pytest.mark.skip(reason='not support for current version')
+    @pytest.mark.parametrize('num_tokens', [1024, 2048])
+    @pytest.mark.parametrize('hidden_size', [6144, 8192, 12288])
+    @pytest.mark.parametrize('topk', [1, 4])
+    @pytest.mark.parametrize('num_experts', [4, 128])
     @pytest.mark.parametrize('dtype', [torch.bfloat16])
-    def test_npu_fused_permute(self, dtype):
-        token_ori = torch.tensor([[1, 1, 1], [2, 2, 2], [3, 3, 3], [0, 0, 0]]).npu().to(dtype)
-        indices_ori = torch.tensor([[0, 4], [4, 3], [4, 2], [1, 1]]).npu()
-        topk = 2
+    def test_permute(self, num_tokens, hidden_size, topk, num_experts, dtype):
+        tols = TOL_MAPPING.get(dtype)
+        token_ori = torch.randn(num_tokens, hidden_size).npu().to(dtype).requires_grad_(True)
+        indices_ori = torch.randint(0, num_experts, (num_tokens, topk)).npu()
+
         token_ori = token_ori.requires_grad_(True)
         token_fused = token_ori.clone().detach().requires_grad_(True)
         indices_fused = indices_ori.clone().detach()
 
-        args = create_test_args(False)
-        set_args(args)
         permuted_tokens_ori, sorted_indices_ori = permute(token_ori, indices_ori, topk=topk)
-
-        args = create_test_args(True)
-        set_args(args)
-        permuted_tokens_fused, sorted_indices_fused = permute(token_fused, indices_fused)
+        permuted_tokens_fused, sorted_indices_fused = fused_permute(token_fused, indices_fused)
         permuted_tokens_fused.backward(torch.ones(permuted_tokens_fused.shape).to(torch.bfloat16).npu())
 
-        tol = 0.004 if dtype == torch.bfloat16 else 0.001
-        assert torch.allclose(permuted_tokens_ori, permuted_tokens_fused, rtol=tol, atol=tol)
+        assert torch.allclose(permuted_tokens_ori, permuted_tokens_fused, **tols)
         # The fusion operator will perform two torch.argsort operations internally
         sorted_indices_ori = torch.argsort(sorted_indices_ori, stable=True).to(sorted_indices_fused.dtype)
         assert torch.equal(sorted_indices_ori, sorted_indices_fused)
 
     @pytest.mark.skip(reason='not support for current version')
+    @pytest.mark.parametrize('num_tokens', [1024, 2048])
+    @pytest.mark.parametrize('hidden_size', [6144, 8192, 12288])
+    @pytest.mark.parametrize('topk', [1, 4])
+    @pytest.mark.parametrize('num_experts', [4, 128])
     @pytest.mark.parametrize('dtype', [torch.bfloat16])
-    def test_npu_fused_unpermute(self, dtype):
-        permuted_tokens_ori = torch.tensor([[1., 1., 1.],
-                                            [0., 0., 0.],
-                                            [0., 0., 0.],
-                                            [3., 3., 3.],
-                                            [2., 2., 2.],
-                                            [1., 1., 1.],
-                                            [2., 2., 2.],
-                                            [3., 3., 3.]]).npu().to(dtype)
-        sorted_indices_ori = torch.tensor([0, 6, 7, 5, 3, 1, 2, 4], dtype=torch.int32).npu()
-        indices = torch.tensor([[0, 4], [4, 3], [4, 2], [1, 1]]).npu()
-        topk = 2
-        probs_ori = torch.ones_like(indices) / 2
-        probs_ori = probs_ori.npu().to(dtype)
-        permuted_tokens_ori = permuted_tokens_ori.requires_grad_(True)
+    def test_unpermute(self, num_tokens, hidden_size, topk, num_experts, dtype):
+        tols = TOL_MAPPING.get(dtype)
+        permuted_tokens_ori = torch.randn(num_tokens * topk, hidden_size).npu().to(dtype)
+        indices = torch.randint(0, num_experts, (num_tokens, topk)).npu()
+        sorted_indices_ori = torch.argsort(indices.view(-1), stable=True).npu().to(dtype=torch.int32)
+        probs_ori = None
+        probs_fused = None
+        if topk > 1:
+            probs_ori = (torch.ones(num_tokens, topk) / topk).npu().to(dtype).requires_grad_(True)
+            probs_fused = probs_ori.clone().detach().requires_grad_(True)
         permuted_tokens_fused = permuted_tokens_ori.clone().detach().requires_grad_(True)
         sorted_indices_fused = sorted_indices_ori.clone().detach()
-        probs_fused = probs_ori.clone().detach().requires_grad_(True)
 
-        args = create_test_args(False)
-        set_args(args)
         # The fusion operator will perform two torch.argsort operations internally
         sorted_indices_ori = torch.argsort(sorted_indices_ori, stable=True)
         unpermuted_tokens_ori = unpermute(
             permuted_tokens_ori, sorted_indices_ori, probs=probs_ori, topk=topk)
 
-        args = create_test_args(True)
-        set_args(args)
-        unpermuted_tokens_fused = unpermute(
-            permuted_tokens_fused, sorted_indices_fused, probs=probs_fused, topk=topk)
+        unpermuted_tokens_fused = fused_unpermute(
+            permuted_tokens_fused, sorted_indices_fused, probs=probs_fused)
 
-        tol = 0.004 if dtype == torch.bfloat16 else 0.001
         unpermuted_tokens_fused.backward(torch.ones(unpermuted_tokens_fused.shape).to(torch.bfloat16).npu())
-        assert torch.allclose(unpermuted_tokens_ori, unpermuted_tokens_fused, rtol=tol, atol=tol)
+        assert torch.allclose(unpermuted_tokens_ori, unpermuted_tokens_fused, **tols)
 
     @pytest.mark.skip(reason='not support for current version')
+    @pytest.mark.parametrize('num_tokens', [1024, 2048])
+    @pytest.mark.parametrize('hidden_size', [6144, 8192, 12288])
+    @pytest.mark.parametrize('topk', [1, 4])
+    @pytest.mark.parametrize('num_experts', [4, 128])
     @pytest.mark.parametrize('dtype', [torch.bfloat16])
-    def test_fused_permute_and_unpermute(self, dtype):
-        tokens = torch.tensor([[1, 1, 1], [2, 2, 2], [3, 3, 3], [0, 0, 0]]).npu().to(dtype)
-        indices = torch.tensor([[0, 4], [4, 3], [4, 2], [1, 1]]).npu()
-        probs = torch.ones_like(indices) / 2
-        probs = probs.npu().to(dtype)
-        args = create_test_args(True)
-        set_args(args)
-        permuted_tokens, sorted_indices = permute(tokens, indices)
-        unpermuted_tokens_fused = unpermute(permuted_tokens, sorted_indices, probs=probs)
-        assert torch.allclose(tokens, unpermuted_tokens_fused)
+    def test_ori_permute_unpermute(self, num_tokens, hidden_size, topk, num_experts, dtype):
+        tols = TOL_MAPPING.get(dtype)
+        tokens = torch.randn(num_tokens, hidden_size).npu().to(dtype)
+        indices = torch.randint(0, num_experts, (num_tokens, topk)).npu()
+        probs = None
+        if topk > 1:
+            probs = (torch.ones_like(indices) / topk).npu().to(dtype)
+
+        permuted_tokens, sorted_indices = permute(tokens, indices, topk=topk)
+        unpermuted_tokens = unpermute(permuted_tokens, sorted_indices, probs=probs, topk=topk)
+
+        if topk == 1:
+            assert torch.equal(unpermuted_tokens, tokens)
+        else:
+            assert torch.allclose(unpermuted_tokens, tokens, **tols)
 
     @pytest.mark.skip(reason='not support for current version')
+    @pytest.mark.parametrize('num_tokens', [1024, 2048])
+    @pytest.mark.parametrize('hidden_size', [6144, 8192, 12288])
+    @pytest.mark.parametrize('topk', [1, 4])
+    @pytest.mark.parametrize('num_experts', [4, 128])
     @pytest.mark.parametrize('dtype', [torch.bfloat16])
-    def test_ori_permute_and_unpermute(self, dtype):
-        tokens = torch.tensor([[1, 1, 1], [2, 2, 2], [3, 3, 3], [0, 0, 0]]).npu().to(dtype)
-        indices = torch.tensor([[0, 4], [4, 3], [4, 2], [1, 1]]).npu()
-        topk = 2
-        probs = torch.ones_like(indices) / 2
-        probs = probs.npu().to(dtype)
-        args = create_test_args(False)
-        set_args(args)
-        permuted_tokens_ori, sorted_indices_ori = permute(tokens, indices, topk=topk)
-        unpermuted_tokens_ori = unpermute(permuted_tokens_ori, sorted_indices_ori, probs=probs)
+    def test_npu_fused_permute_unpermute(self, num_tokens, hidden_size, topk, num_experts, dtype):
+        tols = TOL_MAPPING.get(dtype)
+        tokens = torch.randn(num_tokens, hidden_size).npu().to(dtype)
+        indices = torch.randint(0, num_experts, (num_tokens, topk)).npu()
+        probs = None
+        if topk > 1:
+            probs = (torch.ones_like(indices) / topk).npu().to(dtype)
 
-        assert torch.allclose(tokens, unpermuted_tokens_ori)
+        permuted_tokens, sorted_indices = fused_permute(tokens, indices, topk=topk)
+        unpermuted_tokens = fused_unpermute(permuted_tokens, sorted_indices, probs=probs, topk=topk)
+
+        if topk == 1:
+            assert torch.equal(unpermuted_tokens, tokens)
+        else:
+            assert torch.allclose(unpermuted_tokens, tokens, **tols)
