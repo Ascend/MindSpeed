782021ba12c994b7c01b0d4e0ba7f7313c3002a3
Even Zhou <zhouyiwen8@huawei.com>
2024-11-12 09:05:50 +0000

    !1282 开箱安全整改，README加入说明 Merge pull request !1282 from Even Zhou/at-update

--- mindspeed/auto_tuning/utils/logger.py
+++ mindspeed/auto_tuning/utils/logger.py
@@ -6,27 +6,22 @@ from sys import stdout
 _LOGGERS: Set[str] = set()
 _LOG_FMT = "[%(levelname)s] %(name)s: %(message)s"
 _LOG_LEVEL = logging.INFO
-_LOG_FILE = None
 _LOGGER_NAME_PREFIX = "auto-tuning"
 
 
-def init_logger(level: str = "info", log_file: Optional[str] = None):
-    global _LOG_LEVEL, _LOG_FILE
+def init_logger(level: str = "info"):
+    global _LOG_LEVEL
     if level == "warning":
         _LOG_LEVEL = logging.WARNING
     elif level == "debug":
         _LOG_LEVEL = logging.DEBUG
     else:
         _LOG_LEVEL = logging.INFO
-    if log_file:
-        _LOG_FILE = os.path.abspath(log_file)
 
     for name in _LOGGERS:
         logger_name = f"{_LOGGER_NAME_PREFIX}.{name}"
         logger = logging.getLogger(name=logger_name)
         logger.setLevel(_LOG_LEVEL)
-        if _LOG_FILE:
-            logger.addHandler(logging.FileHandler(_LOG_FILE, encoding="utf-8"))
         for handler in logger.handlers:
             handler.setFormatter(logging.Formatter(fmt=_LOG_FMT))
 
@@ -39,8 +34,6 @@ def get_logger(name: str):
         logger.propagate = False
         logger.setLevel(_LOG_LEVEL)
         logger.addHandler(logging.StreamHandler(stream=stdout))
-        if _LOG_FILE:
-            logger.addHandler(logging.FileHandler(_LOG_FILE, encoding="utf-8"))
         for handler in logger.handlers:
             handler.setFormatter(logging.Formatter(fmt=_LOG_FMT))
         _LOGGERS.add(name)
