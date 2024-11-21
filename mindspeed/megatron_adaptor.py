919f034ef2e91240d81f2874c52b59fc9d1afaff
cx <chenxu140@huawei.com>
2024-11-21 02:25:47 +0000

    !1374 [1.0.0_core_r0.7.0]bugfix:调用save_tensors_hook接口导致weight没有main_grad属性 Merge pull request !1374 from cx/1.0.0_core_r0.7.0

--- mindspeed/megatron_adaptor.py
+++ mindspeed/megatron_adaptor.py
@@ -404,7 +404,12 @@ def mcore_tensor_parallel_adaptation_l1(aspm):
                         checkpoint_backward_wrapper)                        
 
 
-def mcore_tensor_parallel_adaptation(aspm):
+def mcore_tensor_parallel_adaptation(aspm, args):
+    def has_recomputation_or_swap(args):
+        return (args.swap_attention or
+                args.recompute_in_bubble or
+                args.adaptive_recompute_device_swap or
+                args.recompute_in_advance)
     from .core.tensor_parallel.random import checkpoint_wrapper
     from .core.tensor_parallel.random import checkpoint_function_backward
     from .core.tensor_parallel.layers import vocab_parallel_embedding_forward
@@ -421,6 +426,12 @@ def mcore_tensor_parallel_adaptation(aspm):
     aspm.register_patch('megatron.core.tensor_parallel.layers.ColumnParallelLinear.__init__',
                         parallel_linear_init_wrapper)
     aspm.register_patch('megatron.core.tensor_parallel.random.checkpoint', checkpoint_wrapper)
+    if has_recomputation_or_swap(args):
+        from .core.tensor_parallel.layers import linear_forward_main_grad_wrapper, linear_backward_main_grad_wrapper
+        aspm.register_patch('megatron.core.tensor_parallel.layers.LinearWithGradAccumulationAndAsyncCommunication.forward',
+                            linear_forward_main_grad_wrapper)
+        aspm.register_patch('megatron.core.tensor_parallel.layers.LinearWithGradAccumulationAndAsyncCommunication.backward',
+                            linear_backward_main_grad_wrapper)
 
 
 def megatron_legacy_adaptation(aspm):
@@ -868,7 +879,7 @@ def adaptation_l2(aspm, mindspeed_args):
     mcore_optimizer_adapation(aspm)
     mcore_pipeline_parallel_adaptation(aspm, mindspeed_args)
     mcore_multiparam_pipeline_parallel_adaptation(aspm, mindspeed_args)
-    mcore_tensor_parallel_adaptation(aspm)
+    mcore_tensor_parallel_adaptation(aspm, mindspeed_args)
     mcore_transformer_adaptation(aspm)
 
     # megatron legacy
