e20f5ad58aaf484b9f4cd34b77850101ca689f98
杨杰 <yangjie409@h-partners.com>
2025-01-03 06:39:14 +0000

    !1750 【readme&bugfix！！】bugfix Merge pull request !1750 from 杨杰/1.0.RC3_core_r0.6.0

--- mindspeed/arguments.py
+++ mindspeed/arguments.py
@@ -723,7 +723,11 @@ def validate_args_wrapper(validate_args):
         if args.fp16:
             args.gradient_accumulation_fusion = False
             warnings.warn("Unsupported gradient fp16 bf16 for gradient accumulation fusion")
-            
+
+        if not args.use_mcore_models:
+            if args.overlap_param_gather and args.reuse_fp32_param:
+                raise AssertionError('In legacy, `overlap_param_gather` does not support `reuse_fp32_param`.')
+
         from megatron.training.arguments import _print_args
         _print_args('arguments', args, True)
         return args
