52292dec686632d69705be4e4b9721dcf7e1dc6b
Jializheng <jializheng@huawei.com>
2025-02-05 03:42:52 +0000

    !1833 add profiler group name Merge pull request !1833 from Jializheng/master

--- mindspeed/features_manager/__init__.py
+++ mindspeed/features_manager/__init__.py
@@ -1,5 +1,9 @@
 from .tensor_parallel.unaligned_linear_feature import UnalignedLinearFeature
+from .functional.profiler_default_feature import ProfilerDefaultFeature
 
 FEATURES_LIST = [
-    UnalignedLinearFeature('unaligned-linear')
+    # Functional features
+    ProfilerDefaultFeature(),
+    # Tensor parallel features
+    UnalignedLinearFeature()
 ]
