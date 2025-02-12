93300741d3e70e59477f74036cc4cc71ea195e9d
yuqi <yuqi34@huawei-partners.com>
2025-02-12 08:35:11 +0000

    !1878 【bugfix】修复norm_recompute特性在eval阶段的报错 Merge pull request !1878 from yuqi/norm_fix

--- mindspeed/core/transformer/transformer.py
+++ mindspeed/core/transformer/transformer.py
@@ -275,7 +275,7 @@ def norm_recompute_forward(
 
     if self.training:
         self.norm_ckpt2.discard_output()
-    mlp_output_with_bias[0].register_hook(self.norm_ckpt2.recompute)
+        mlp_output_with_bias[0].register_hook(self.norm_ckpt2.recompute)
 
     with self.bias_dropout_add_exec_handler():
         hidden_states = self.mlp_bda(self.training, self.config.bias_dropout_fusion)(
