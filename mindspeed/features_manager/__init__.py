2e66f47037efc99c709acb14830da9125505b1fb
Jializheng <jializheng@huawei.com>
2025-03-28 09:51:40 +0000

    !2123 Swap Optimizer Merge pull request !2123 from Jializheng/2.0.0_core_r0.8.0

--- mindspeed/features_manager/__init__.py
+++ mindspeed/features_manager/__init__.py
@@ -1,7 +1,9 @@
 from .tensor_parallel.unaligned_linear_feature import UnalignedLinearFeature
 from .functional.profiler_default_feature import ProfilerDefaultFeature
+from .optimizer.swap_optimizer_feature import SwapOptimizerFeature
 
 FEATURES_LIST = [
+    SwapOptimizerFeature(),
     # Functional features
     ProfilerDefaultFeature(),
     # Tensor parallel features
