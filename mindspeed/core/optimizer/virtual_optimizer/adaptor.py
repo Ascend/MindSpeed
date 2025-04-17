e53500dc11cd76f6a0f4f07f97c49d8abe87a8b4
Kingsleyandher <lijiawei70@huawei.com>
2025-04-17 01:50:20 +0000

    !2186 feat: Add virtual optimizer Merge pull request !2186 from Kingsleyandher/core_r0.8.0

---
+++ mindspeed/core/optimizer/virtual_optimizer/adaptor.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2025, Huawei Technologies Co., Ltd. All rights reserved.
+
+import torch
+from megatron.core.parallel_state import get_pipeline_model_parallel_rank, get_pipeline_model_parallel_world_size
+from megatron.training import get_args
+from mindspeed.core.optimizer.virtual_optimizer.virtual_adam import virtual_optimizer_step_impl, VirtualAllocator
+
+
+def virtual_optimizer_step(self, closure=None):
+    if not hasattr(self, "virtual_allocator"):
+        self.virtual_allocator = VirtualAllocator(
+            get_pipeline_model_parallel_rank(),
+            get_pipeline_model_parallel_world_size(),
+            get_args().virtual_optimizer)
+    with torch.no_grad():
+        loss = virtual_optimizer_step_impl(self, closure)
+    return loss
--- mindspeed/features_manager/__init__.py
+++ mindspeed/features_manager/__init__.py
@@ -4,9 +4,11 @@ from .pipeline_parallel.fb_overlap_feature import FwdBwdOverlapFeature
 from .optimizer.swap_optimizer_feature import SwapOptimizerFeature
 from .pipeline_parallel.fb_overlap_feature import FwdBwdOverlapFeature
 from .pipeline_parallel.dualpipev_feature import DualpipeVFeature
+from .optimizer.virtual_optimizer import VirtualOptimizerFeature
 
 FEATURES_LIST = [
     SwapOptimizerFeature(),
+    VirtualOptimizerFeature(),
     # Functional features
     ProfilerDefaultFeature(),
     # Tensor parallel features
