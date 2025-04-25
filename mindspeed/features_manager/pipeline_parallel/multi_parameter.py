377171ce3a6180a229658d57bc762fdbbc37e692
liurong1995 <liurong55@huawei.com>
2025-04-25 06:08:16 +0000

    !2224 重构：pp支持多shape参数传递 Merge pull request !2224 from liurong1995/feature_multiparameter

---
+++ mindspeed/features_manager/pipeline_parallel/multi_parameter.py
@@ -0,0 +1,97 @@
+"""Define multi parameter feature of pipeline parallel training.
+
+Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+Copyright (c) 2025, Huawei Technologies Co., Ltd. All rights reserved.
+"""
+
+from argparse import ArgumentParser, Namespace
+
+from mindspeed.features_manager.feature import MindSpeedFeature
+from mindspeed.patch_utils import MindSpeedPatchesManager
+
+
+class MultiParameterFeature(MindSpeedFeature):
+    """Multi parameter feature of pipeline parallel training."""
+
+    def __init__(
+        self,
+        feature_name: str = "use-multiparameter-pipeline-model-parallel",
+        optimization_level: int = 0,
+    ):
+        super().__init__(feature_name, optimization_level)
+
+    def register_args(self, parser: ArgumentParser):
+        group = parser.add_argument_group(title=self.feature_name)
+        group.add_argument(
+            "--use-multiparameter-pipeline-model-parallel",
+            action="store_true",
+            default=False,
+            help="can transfer multi parameters from "
+            "stage to stage in pipeline model parallel",
+        )
+
+    def register_patches(
+        self,
+        patch_manager: MindSpeedPatchesManager,
+        args: Namespace,
+    ):
+        # pylint: disable=import-outside-toplevel
+        from mindspeed.core.pipeline_parallel.multi_parameter.adaptor import (
+            get_tensor_shapes_wrapper,
+            forward_step_wrapper,
+            mindspeed_backward_step,
+            mindspeed_recv_forward,
+            mindspeed_recv_backward,
+            mindspeed_send_forward,
+            mindspeed_send_backward,
+            mindspeed_send_forward_recv_backward,
+            mindspeed_send_backward_recv_forward,
+            get_forward_backward_func_wrapper,
+            core_transformer_config_from_args_wrapper,
+        )
+
+        if getattr(args, self.feature_name, None):
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.get_tensor_shapes",
+                get_tensor_shapes_wrapper,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.forward_step",
+                forward_step_wrapper,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.backward_step",
+                mindspeed_backward_step,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.recv_forward",
+                mindspeed_recv_forward,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.recv_backward",
+                mindspeed_recv_backward,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.send_forward",
+                mindspeed_send_forward,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.send_backward",
+                mindspeed_send_backward,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.send_forward_recv_backward",  # noqa
+                mindspeed_send_forward_recv_backward,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.send_backward_recv_forward",  # noqa
+                mindspeed_send_backward_recv_forward,
+            )
+            patch_manager.register_patch(
+                "megatron.core.pipeline_parallel.schedules.get_forward_backward_func",  # noqa
+                get_forward_backward_func_wrapper,
+            )
+            patch_manager.register_patch(
+                "megatron.training.arguments.core_transformer_config_from_args",  # noqa
+                core_transformer_config_from_args_wrapper,
+            )
