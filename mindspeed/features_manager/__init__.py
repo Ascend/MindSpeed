59c9184b2ce557b52832f156e73c3623b80bad91
抄小抄 <1069958308@qq.com>
2025-04-28 02:00:41 +0000

    !2244 feat: dense层激活值压缩 -core_r0.8.0 Merge pull request !2244 from 抄小抄/core_r0.8.0

--- mindspeed/features_manager/__init__.py
+++ mindspeed/features_manager/__init__.py
@@ -5,11 +5,13 @@ from .optimizer.swap_optimizer_feature import SwapOptimizerFeature
 from .pipeline_parallel.fb_overlap_feature import FwdBwdOverlapFeature
 from .pipeline_parallel.dualpipev_feature import DualpipeVFeature
 from .optimizer.virtual_optimizer import VirtualOptimizerFeature
+from .compress_dense.compress_dense import AnsCompressTensorFeature
 from .moe.moe_zerc_feature import MoeZeRCFeature
 
 FEATURES_LIST = [
     SwapOptimizerFeature(),
     VirtualOptimizerFeature(),
+    AnsCompressTensorFeature(),
     # Functional features
     ProfilerDefaultFeature(),
     # Tensor parallel features
