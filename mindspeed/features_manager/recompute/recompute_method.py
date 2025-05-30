5af040c92a82be1b2d29d03eefcbfc61f830b776
邓佳 <dengjia6@huawei.com>
2025-05-30 08:43:07 +0000

    !2364 add --use-fused-swiglu;  fix: RecomputeMethod Merge pull request !2364 from 邓佳/master_fix_v5

--- mindspeed/features_manager/recompute/recompute_method.py
+++ mindspeed/features_manager/recompute/recompute_method.py
@@ -15,7 +15,7 @@ class RecomputeMethodFeature(MindSpeedFeature):
 
     def register_basic_patches(self, patch_manager, args):
         from mindspeed.core.memory.common import transformer_block_checkpointed_forward
-        if (getattr(args, 'recompute_method', False) and args.recompute_method == 'block'):
+        if (getattr(args, 'recompute_method', False) and args.recompute_method == 'block') and not getattr(args, 'swap_attention', False):
             patch_manager.register_patch(
                 'megatron.core.transformer.transformer_block.TransformerBlock._checkpointed_forward',
                 transformer_block_checkpointed_forward)
