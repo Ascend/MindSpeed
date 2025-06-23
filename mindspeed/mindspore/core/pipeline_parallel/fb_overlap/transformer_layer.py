05a3245ecc0911cafc63b78927fd4052ba3cd489
杨承翰 <handsome0324@163.com>
2025-06-23 11:44:43 +0000

    !2485 [mindspore][bugfix]Fix error enabling moe-zerc + dualpipe Merge pull request !2485 from 杨承翰/bugfix_core_r0.8.0

--- mindspeed/mindspore/core/pipeline_parallel/fb_overlap/transformer_layer.py
+++ mindspeed/mindspore/core/pipeline_parallel/fb_overlap/transformer_layer.py
@@ -1,35 +1,35 @@
 #  Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
 
-from contextlib import nullcontext
 import torch
-from mindspeed.core.transformer.transformer_block import NoopTransformerLayer
-
+from megatron.training.utils import get_args
+from mindspeed.utils import set_actual_seq_len, set_position_ids, get_actual_seq_len, get_position_ids
 from mindspeed.core.pipeline_parallel.fb_overlap.transformer_layer import transformer_layer_forward
 
-from mindspeed.core.pipeline_parallel.fb_overlap.modules.utils import NoopLayerGraph
 
-from mindspeed.mindspore.core.pipeline_parallel.fb_overlap.overlap_funcs import (
-    transformer_layer_backward_moe,
-    transformer_layer_backward_dense,
-    transformer_layer_backward_noop,
-)
-
-
-
-def transformer_layer_backward(
-    layer_output_grad,
-    layer_graph
+def transformer_layer_recompute(
+        bwd_layer_graph
 ):
-    if layer_graph.checkpointed:
-        with torch.enable_grad():
-            _, _, restored_layer_graph = transformer_layer_forward(
-                layer_graph.layer, layer_graph.layer_input, *layer_graph.layer_inputs, checkpoint=False
-            )
-            restored_layer_graph.unperm2_graph = (restored_layer_graph.unperm2_graph[0], layer_graph.unperm2_graph[1], restored_layer_graph.unperm2_graph[2])
-            layer_graph = restored_layer_graph
-    if isinstance(layer_graph, NoopLayerGraph):
-        return transformer_layer_backward_noop(layer_output_grad, layer_graph)
-    elif layer_graph.is_moe_layer:
-        return transformer_layer_backward_moe(layer_output_grad, layer_graph)
-    else:
-        return transformer_layer_backward_dense(layer_output_grad, layer_graph)
+    if not bwd_layer_graph.checkpointed:
+        return bwd_layer_graph
+    # Set actual_seq_len for recompute and avoid using actual_seq_len in forward context.
+    # Also Record the forward context for restore.
+    if get_args().reset_position_ids:
+        fwd_actual_seq_len = get_actual_seq_len()
+        fwd_position_ids = get_position_ids()
+        set_actual_seq_len(bwd_layer_graph.actual_seq_len)
+        set_position_ids(bwd_layer_graph.position_ids)
+
+    # Recompute entire transformer layer for backward.
+    with torch.enable_grad():
+        _, _, restored_layer_graph = transformer_layer_forward(
+            bwd_layer_graph.layer, bwd_layer_graph.layer_input, *bwd_layer_graph.layer_inputs, checkpoint=False
+        )
+        restored_layer_graph.unperm2_graph = (restored_layer_graph.unperm2_graph[0], bwd_layer_graph.unperm2_graph[1], restored_layer_graph.unperm2_graph[2])
+
+    # Restore acutal_seq_len in forward context.
+    if get_args().reset_position_ids:
+        set_actual_seq_len(fwd_actual_seq_len)
+        set_position_ids(fwd_position_ids)
+
+    return restored_layer_graph
+
