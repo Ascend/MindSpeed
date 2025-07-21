!2654 fix recompute policy list overflow Merge pull request !2654 from wangyuansheng8/2.1.0_core_r0.12.1

mindspeed/core/memory/adaptive_recomputing/adaptive_recompute_solver.py
@@ -152,10 +152,10 @@ class GraphSolver:
         self.broadcast_in_mp_dp(policy_len_tensor, src)
         policy_len = tuple(policy_len_tensor.cpu().numpy().tolist())
         if global_rank == src:
-            recompute_policy_tensor = torch.tensor(recompute_policy_list, dtype=torch.int8,
+            recompute_policy_tensor = torch.tensor(recompute_policy_list, dtype=torch.int32,
                                                    device=torch.npu.current_device())
         else:
-            recompute_policy_tensor = torch.empty(policy_len, dtype=torch.int8,
+            recompute_policy_tensor = torch.empty(policy_len, dtype=torch.int32,
                                                   device=torch.npu.current_device())
 
         self.broadcast_in_mp_dp(recompute_policy_tensor, src)