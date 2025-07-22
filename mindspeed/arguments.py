41b65c32eaa4f3fab52d8c02d8f32b511cb7bf59
wangyuansheng8 <wangyuansheng8@h-partners.com>
2025-07-22 06:38:04 +0000

    !2668 fix lora target modules assertion bug Merge pull request !2668 from wangyuansheng8/core_r0.9.0

--- mindspeed/arguments.py
+++ mindspeed/arguments.py
@@ -949,7 +949,7 @@ def validate_args_wrapper(validate_args):
                 raise AssertionError('Only ckpt-format = torch is supported.')
 
         if args.swap_attention and hasattr(args, "lora_target_modules"):
-            if args.lora_target_modules is not []:
+            if len(args.lora_target_modules) != 0:
                 raise AssertionError('swap attention is not compatible with LoRA')
 
         from megatron.training.arguments import _print_args
