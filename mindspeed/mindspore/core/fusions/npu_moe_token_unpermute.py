c6bd1c11ab030011440042b247e6128849dfcaa7
杨承翰 <handsome0324@163.com>
2025-09-08 01:28:50 +0000

    !2812 [mindspore][update][master]update useless wrapper and add transformers patch Merge pull request !2812 from 杨承翰/dispatch_master

--- mindspeed/mindspore/core/fusions/npu_moe_token_unpermute.py
+++ mindspeed/mindspore/core/fusions/npu_moe_token_unpermute.py
@@ -6,20 +6,16 @@ import torch
 from mindspeed.mindspore.ops.npu_moe_token_unpermute import npu_moe_token_unpermute
 
 
-def unpermute_wrapper(fn):
-    @wraps(fn)
-    def wrapper(
-        permuted_tokens: torch.Tensor,
+def unpermute(permuted_tokens: torch.Tensor,
         sorted_indices: torch.Tensor,
         probs: torch.Tensor = None,
         padded_mode: bool = False,
-        restore_shape: torch.Size = None,
-) -> torch.Tensor:
-        dtype = permuted_tokens.dtype
-        if probs is not None and probs.dtype != permuted_tokens.dtype:
-            # make sure permuted_tokens has the same dtype with probs.
-            permuted_tokens = permuted_tokens.to(probs.dtype)
-        return npu_moe_token_unpermute(
-                permuted_tokens, sorted_indices, probs, padded_mode=padded_mode, restore_shape=restore_shape).to(dtype)
+        restore_shape: torch.Size = None):
 
-    return wrapper
+    dtype = permuted_tokens.dtype
+    if probs is not None and probs.dtype != permuted_tokens.dtype:
+        # make sure permuted_tokens has the same dtype with probs.
+        permuted_tokens = permuted_tokens.to(probs.dtype)
+    return npu_moe_token_unpermute(
+            permuted_tokens, sorted_indices, probs, padded_mode=padded_mode, restore_shape=restore_shape).to(dtype)
+
