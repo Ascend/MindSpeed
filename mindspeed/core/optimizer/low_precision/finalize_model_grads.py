5d713c345b0d9b62b919cbfe2c3be49e3a1be2f9
w30064656 <wangzhuangzhuang8@h-partners.com>
2025-11-19 18:05:14 +0800

    quant fp8 optimizer

--- mindspeed/core/optimizer/low_precision/finalize_model_grads.py
+++ mindspeed/core/optimizer/low_precision/finalize_model_grads.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2024; NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2024; NVIDIA CORPORATION.
+# Copyright (c) 2024, Huawei Technologies Co., Ltd.  All rights reserved.
+
 import os
 from typing import List
 
--- mindspeed/core/optimizer/low_precision/quant_grad_clip.py
+++ mindspeed/core/optimizer/low_precision/quant_grad_clip.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2024; NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2024; NVIDIA CORPORATION.
+# Copyright (c) 2024, Huawei Technologies Co., Ltd.  All rights reserved.
+
 from functools import wraps
 from typing import List, Optional, Union
 
