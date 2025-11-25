419f310ee5f10ece1ffa09cad13e10176bd2acc6
ascend-robot <zhongyuanke@huawei.com>
2025-11-25 14:11:31 +0800

    Add chunk_delta_h Triton operators and test cases.
    Co-authored-by: 王雪飞<18800205069@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3046 merge 2.3.0_core_r0.12.1 into 2.3.0_core_r0.12.1
    
    Add chunk_delta_h Triton operators and test cases.
    
    Created-by: Ling_i
    Commit-by: 王雪飞
    Merged-by: ascend-robot
    Description: Add chunk_delta_h Triton operators and test cases.
    
    See merge request: Ascend/MindSpeed!3046

--- mindspeed/lite/ops/triton/utils.py
+++ mindspeed/lite/ops/triton/utils.py
@@ -214,3 +214,11 @@ def check_shared_mem(arch: str = "none", tensor_idx: int = 0) -> bool:
         return max_shared_memory >= Backend.get_shared_memory(arch)
     except Exception:
         return False
+    
+
+@tensor_cache
+def prepare_chunk_offsets(
+    cu_seqlens: torch.LongTensor,
+    chunk_size: int
+) -> torch.LongTensor:
+    return torch.cat([cu_seqlens.new_tensor([0]), triton.cdiv(prepare_lens(cu_seqlens), chunk_size)]).cumsum(-1)
