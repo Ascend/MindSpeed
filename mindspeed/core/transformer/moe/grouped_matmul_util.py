fix(gmmadd): fix high-precision gmmadd
Co-authored-by: xu_yuanhui<xuyuanhui3@h-partners.com>



# message auto-generated for no-merge-commit merge:
!3224 merge master into master

fix(gmmadd): fix high-precision gmmadd

Created-by: xu_yuanhui
Commit-by: xu_yuanhui
Merged-by: ascend-robot
Description: fix high-precision gmmadd

See merge request: Ascend/MindSpeed!3224

mindspeed/core/transformer/moe/grouped_matmul_util.py
@@ -133,7 +133,10 @@ class BaseGMMFunction(torch.autograd.Function):
 
     @classmethod
     def gmm_add_impl(cls, x, grad, group_list, weight_param, weight_shape):
-        npu_groupmatmul_add_fp32(x, grad, group_list, weight_param.main_grad.view(weight_shape))
+        if is_a5():
+            npu_groupmatmul_add_fp32(x, grad, group_list, weight_param.main_grad.view(weight_shape))
+        else:
+            npu_groupmatmul_add_fp32(x, grad, group_list, weight_param.main_grad)
 
 
 class BF16GMMFunction(BaseGMMFunction):