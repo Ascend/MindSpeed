71b84b0fac601e011b156597abbf737812a5ece0
Muuyo <koimuu@163.com>
2026-02-04 17:50:35 +0800

    feature(ut): uses third-party verification for mc2 UT.
    Co-authored-by: Muu<koimuu@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3247 merge feature_mc2 into master
    
    feature(ut): uses third-party verification for mc2 UT.
    
    Created-by: Muuyo
    Commit-by: Muu
    Merged-by: ascend-robot
    Description: 采用三方验证 
    输入值域 [-5,5]
    验证文档: https://wiki.huawei.com/domains/76578/wiki/233229/WIKI2026013110011445
    
    See merge request: Ascend/MindSpeed!3247

--- tests_extend/unit_tests/mindspeed/te/test_coc.py
+++ tests_extend/unit_tests/mindspeed/te/test_coc.py
@@ -1,9 +1,10 @@
 import pytest
+
 import torch
 import torch_npu
+
 from torch.nn.parameter import Parameter
 import mindspeed.megatron_adaptor
-
 from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParallelLinear
 from megatron.training.arguments import parse_args, core_transformer_config_from_args
 from megatron.training.global_vars import set_args
@@ -14,6 +15,7 @@ from mindspeed.te.pytorch.module.ops.default_ops import DefaultOps
 from mindspeed.te.pytorch.module.ops.mc2_ops import Mc2Ops
 from tests_extend.commons import initialize_model_parallel
 from tests_extend.unit_tests.common import DistributedTest
+from tests_extend.unit_tests.utils import multi_compare
 
 
 class TestAllgatherMatmul(DistributedTest):
@@ -30,13 +32,23 @@ class TestAllgatherMatmul(DistributedTest):
         set_args(args)
         initialize_model_parallel(self.world_size, 1)
         _set_random_seed(seed_=123, data_parallel_random_init=False)
-
-        x = torch.randn(seq_size, batch_size, input_size, dtype=dtype).npu()
-        w = torch.randn(seq_size, input_size, dtype=dtype).npu()
+        x = torch.randn(seq_size, batch_size, input_size, dtype=dtype) * 5
+        w = torch.randn(seq_size, input_size, dtype=dtype) * 5
         fp8_meta = FP8Metadata(["inputs", "weight", "grads"])
-        output_baseline = DefaultOps.allgather_matmul(x, w.t(), None, fp8_meta)
-        output_mc2 = Mc2Ops.allgather_matmul(x, w.t(), None, fp8_meta)
-        assert torch.allclose(output_baseline[0], output_mc2[0], rtol=0.005, atol=0.005)
+        output_baseline = self.allgather_matmul(x, w).view(-1)
+        output_default = DefaultOps.allgather_matmul(x.npu(), w.npu(), None, fp8_meta, transpose=(False, True))
+        output_mc2 = Mc2Ops.allgather_matmul(x.npu(), w.npu(), None, fp8_meta, transpose=(False, True))
+        output_default = output_default[0].view(-1).cpu()
+        output_mc2 = output_mc2[0].view(-1).cpu()
+        assert multi_compare(output_mc2, output_baseline, output_default, f"{torch.npu.current_device()}") != "FAIL"
+
+    def allgather_matmul(self, input_, weight):
+        dim_size = list(input_.size())
+        dim_size[0] = dim_size[0] * self.world_size
+        total_input = torch.empty(dim_size, dtype=input_.dtype, device=input_.device)
+        group = torch.distributed.new_group(list(range(self.world_size)), backend="gloo")
+        torch.distributed._all_gather_base(total_input, input_.contiguous(), group=group, async_op=False)
+        return torch.matmul(total_input, weight.t())
 
 
 class TestMatmulReduceScatter(DistributedTest):
@@ -54,12 +66,30 @@ class TestMatmulReduceScatter(DistributedTest):
         initialize_model_parallel(self.world_size, 1)
         _set_random_seed(seed_=123, data_parallel_random_init=False)
 
-        x = torch.randn(seq_size, batch_size, input_size, dtype=dtype).npu()
-        w = torch.randn(seq_size, input_size, dtype=dtype).npu()
+        x = torch.randn(seq_size, batch_size, input_size, dtype=dtype) * 5
+        w = torch.randn(seq_size, input_size, dtype=dtype) * 5
         fp8_meta = FP8Metadata(["inputs", "weight", "grads"])
-        output_baseline, _, _ = DefaultOps.matmul_reduce_scatter(x, w, None, fp8_meta)
-        output_mc2, _, _ = Mc2Ops.matmul_reduce_scatter(x, w, None, fp8_meta)
-        assert torch.allclose(output_baseline, output_mc2, rtol=0.005, atol=0.005)
+        output_baseline = self.reduce_scatter(x, w).view(-1)
+        output_default, _, _ = DefaultOps.matmul_reduce_scatter(x.npu(), w.npu(), None, fp8_meta)
+        output_mc2, _, _ = Mc2Ops.matmul_reduce_scatter(x.npu(), w.npu(), None, fp8_meta)
+        output_default = output_default.view(-1).cpu()
+        output_mc2 = output_mc2.view(-1).cpu()
+        assert multi_compare(
+            output_mc2,
+            output_baseline,
+            output_default,
+            f"{torch.npu.current_device()}",
+            "l0"
+        ) != "FAIL"
+
+    def reduce_scatter(self, x, w):
+        output_ = torch.matmul(x, w.t())
+        dim_size = list(output_.size())
+        dim_size[0] = dim_size[0] // self.world_size
+        output = torch.empty(dim_size, dtype=output_.dtype)
+        group = torch.distributed.new_group(list(range(self.world_size)), backend="gloo")
+        torch.distributed._reduce_scatter_base(output, output_.contiguous(), group=group)
+        return output
 
 
 class TestMatmulAllReduce(DistributedTest):
@@ -77,24 +107,42 @@ class TestMatmulAllReduce(DistributedTest):
         initialize_model_parallel(self.world_size, 1)
         _set_random_seed(seed_=123, data_parallel_random_init=False)
 
-        x = torch.randn(seq_size, batch_size, input_size, dtype=dtype).npu()
-        w = torch.randn(seq_size, input_size, dtype=dtype).npu()
+        x = torch.randn(seq_size, batch_size, input_size, dtype=dtype) * 5
+        w = torch.randn(seq_size, input_size, dtype=dtype) * 5
         fp8_meta = FP8Metadata(["inputs", "weight", "grads"])
-        output_baseline, _, _ = DefaultOps.matmul_all_reduce(x, w, None, fp8_meta)
-        output_mc2, _, _ = Mc2Ops.matmul_all_reduce(x, w, None, fp8_meta)
-        assert torch.allclose(output_baseline, output_mc2, rtol=0.005, atol=0.005)
+        output_baseline = self.matmul_all_reduce(x, w).view(-1)
+        output_default, _, _ = DefaultOps.matmul_all_reduce(x.npu(), w.npu(), None, fp8_meta)
+        output_mc2, _, _ = Mc2Ops.matmul_all_reduce(x.npu(), w.npu(), None, fp8_meta)
+        output_default = output_default.view(-1).cpu()
+        output_mc2 = output_mc2.view(-1).cpu()
+        assert multi_compare(
+            output_mc2,
+            output_baseline,
+            output_default,
+            f"{torch.npu.current_device()}",
+            "l0"
+        ) != "FAIL"
+
+    def matmul_all_reduce(self, x, w):
+        output = torch.matmul(x, w.t())
+        group = torch.distributed.new_group(list(range(self.world_size)), backend="gloo")
+        torch.distributed.all_reduce(output, group=group)
+        return output
 
 
 class TestTEColumnParallel(DistributedTest):
     world_size = 8
 
-    @pytest.mark.parametrize('use_ascend_mc2', [True, False])
-    def test_te_column_parallel(self, use_ascend_mc2):
+    @pytest.mark.parametrize("use_ascend_mc2", [True, False])
+    @pytest.mark.parametrize("limit_args", [
+        (torch.bfloat16, 0.005, 0.005)
+    ])
+    def test_te_column_parallel(self, use_ascend_mc2, limit_args):
         batch_size = 1
         seq_size = 4096
         input_size = 1024
         output_size = 1024
-        dtype = torch.bfloat16
+        dtype, rtol, atol = limit_args
 
         args = parse_args(None, True)
         args.params_dtype = dtype
@@ -123,7 +171,8 @@ class TestTEColumnParallel(DistributedTest):
             gather_output=False,
             bias=False,
             skip_bias_add=False,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear = TEColumnParallelLinear(
             input_size=input_size,
             output_size=output_size,
@@ -132,7 +181,8 @@ class TestTEColumnParallel(DistributedTest):
             gather_output=False,
             bias=False,
             skip_bias_add=False,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear.weight = Parameter(linear.weight.clone())
 
         outputs = linear(inputs)
@@ -141,20 +191,23 @@ class TestTEColumnParallel(DistributedTest):
         outputs[0].sum().backward()
         teoutputs[0].sum().backward()
 
-        assert torch.allclose(outputs[0], teoutputs[0], rtol=0.005, atol=0.005)
-        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=0.005, atol=0.005)
+        assert torch.allclose(outputs[0], teoutputs[0], rtol=rtol, atol=atol)
+        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=rtol, atol=atol)
 
 
 class TestTEColumnParallelNoSeq(DistributedTest):
     world_size = 8
 
-    @pytest.mark.parametrize('use_ascend_mc2', [True, False])
-    def test_te_column_parallel_no_seq(self, use_ascend_mc2):
+    @pytest.mark.parametrize("use_ascend_mc2", [True, False])
+    @pytest.mark.parametrize("limit_args", [
+        (torch.bfloat16, 0.005, 0.005)
+    ])
+    def test_te_column_parallel_no_seq(self, use_ascend_mc2, limit_args):
         batch_size = 1
         seq_size = 4096
         input_size = 1024
         output_size = 1024
-        dtype = torch.bfloat16
+        dtype, rtol, atol = limit_args
 
         args = parse_args(None, True)
         args.params_dtype = dtype
@@ -183,7 +236,8 @@ class TestTEColumnParallelNoSeq(DistributedTest):
             gather_output=False,
             bias=False,
             skip_bias_add=False,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear = TEColumnParallelLinear(
             input_size=input_size,
             output_size=output_size,
@@ -192,7 +246,8 @@ class TestTEColumnParallelNoSeq(DistributedTest):
             gather_output=False,
             bias=False,
             skip_bias_add=False,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear.weight = Parameter(linear.weight.clone())
 
         outputs = linear(inputs)
@@ -201,20 +256,23 @@ class TestTEColumnParallelNoSeq(DistributedTest):
         outputs[0].sum().backward()
         teoutputs[0].sum().backward()
 
-        assert torch.allclose(outputs[0], teoutputs[0], rtol=0.005, atol=0.005)
-        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=0.005, atol=0.005)
+        assert torch.allclose(outputs[0], teoutputs[0], rtol=rtol, atol=atol)
+        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=rtol, atol=atol)
 
 
 class TestTERowParallel(DistributedTest):
     world_size = 8
 
-    @pytest.mark.parametrize('use_ascend_mc2', [True, False])
-    def test_te_row_parallel(self, use_ascend_mc2):
+    @pytest.mark.parametrize("use_ascend_mc2", [True, False])
+    @pytest.mark.parametrize("limit_args", [
+        (torch.bfloat16, 0.005, 0.005)
+    ])
+    def test_te_row_parallel(self, use_ascend_mc2, limit_args):
         batch_size = 1
         seq_size = 4096
         input_size = 2048
         output_size = 4096
-        dtype = torch.bfloat16
+        dtype, rtol, atol = limit_args
 
         args = parse_args(None, True)
         args.params_dtype = dtype
@@ -233,8 +291,9 @@ class TestTERowParallel(DistributedTest):
         initialize_model_parallel(self.world_size, 1)
         _set_random_seed(seed_=123, data_parallel_random_init=False)
 
-        inputs = torch.rand(seq_size, batch_size, input_size // args.tensor_model_parallel_size, requires_grad=True,
-                            dtype=dtype).npu()
+        inputs = torch.rand(
+            seq_size, batch_size, input_size // args.tensor_model_parallel_size, requires_grad=True, dtype=dtype
+        ).npu()
         teinputs = inputs.clone()
 
         linear = RowParallelLinear(
@@ -245,7 +304,8 @@ class TestTERowParallel(DistributedTest):
             bias=False,
             input_is_parallel=True,
             skip_bias_add=True,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear = TERowParallelLinear(
             input_size=input_size,
             output_size=output_size,
@@ -254,7 +314,8 @@ class TestTERowParallel(DistributedTest):
             bias=False,
             input_is_parallel=True,
             skip_bias_add=True,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear.weight = Parameter(linear.weight.clone())
 
         outputs = linear(inputs)
@@ -264,20 +325,23 @@ class TestTERowParallel(DistributedTest):
         outputs[0].backward(y_grad)
         teoutputs[0].backward(y_grad)
 
-        assert torch.allclose(outputs[0], teoutputs[0], rtol=0.005, atol=0.005)
-        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=0.005, atol=0.005)
+        assert torch.allclose(outputs[0], teoutputs[0], rtol=rtol, atol=atol)
+        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=rtol, atol=atol)
 
 
 class TestTERowParallelNoSeq(DistributedTest):
     world_size = 8
 
-    @pytest.mark.parametrize('use_ascend_mc2', [True, False])
-    def test_te_row_parallel_no_seq(self, use_ascend_mc2):
+    @pytest.mark.parametrize("use_ascend_mc2", [True, False])
+    @pytest.mark.parametrize("limit_args", [
+        (torch.bfloat16, 0.005, 0.005)
+    ])
+    def test_te_row_parallel_no_seq(self, use_ascend_mc2, limit_args):
         batch_size = 1
         seq_size = 4096
         input_size = 2048
         output_size = 4096
-        dtype = torch.bfloat16
+        dtype, rtol, atol = limit_args
 
         args = parse_args(None, True)
         args.params_dtype = dtype
@@ -296,8 +360,9 @@ class TestTERowParallelNoSeq(DistributedTest):
         initialize_model_parallel(self.world_size, 1)
         _set_random_seed(seed_=123, data_parallel_random_init=False)
 
-        inputs = torch.rand(seq_size, batch_size, input_size // args.tensor_model_parallel_size, requires_grad=True,
-                            dtype=dtype).npu()
+        inputs = torch.rand(
+            seq_size, batch_size, input_size // args.tensor_model_parallel_size, requires_grad=True, dtype=dtype
+        ).npu()
         teinputs = inputs.clone()
 
         linear = RowParallelLinear(
@@ -308,7 +373,8 @@ class TestTERowParallelNoSeq(DistributedTest):
             bias=False,
             input_is_parallel=True,
             skip_bias_add=True,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear = TERowParallelLinear(
             input_size=input_size,
             output_size=output_size,
@@ -317,7 +383,8 @@ class TestTERowParallelNoSeq(DistributedTest):
             bias=False,
             input_is_parallel=True,
             skip_bias_add=True,
-            is_expert=False)
+            is_expert=False,
+        )
         telinear.weight = Parameter(linear.weight.clone())
 
         outputs = linear(inputs)
@@ -327,5 +394,5 @@ class TestTERowParallelNoSeq(DistributedTest):
         outputs[0].backward(y_grad)
         teoutputs[0].backward(y_grad)
 
-        assert torch.allclose(outputs[0], teoutputs[0], rtol=0.005, atol=0.005)
-        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=0.005, atol=0.005)
+        assert torch.allclose(outputs[0], teoutputs[0], rtol=rtol, atol=atol)
+        assert torch.allclose(linear.weight.grad, telinear.weight.grad, rtol=rtol, atol=atol)
