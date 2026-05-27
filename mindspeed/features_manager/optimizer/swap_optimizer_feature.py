9ba787fdfd05d1cbd4cfd004e65d9e1aa0d02adc
JialiZheng1 <jializheng@huawei.com>
2026-05-27 17:57:26 +0800

    feat: Add swap muon
    Co-authored-by: JialiZheng<jializheng@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3502 merge master into master
    
    feat: Add swap muon
    
    Created-by: JialiZheng1
    Commit-by: JialiZheng
    Merged-by: ascend-robot
    Description: Add swap muon
    验证 wiki：https://wiki.huawei.com/wiki/WIKI2026052611229978
    RFC：https://gitcode.com/Ascend/MindSpeed/issues/164
    
    See merge request: Ascend/MindSpeed!3502

--- mindspeed/features_manager/optimizer/swap_optimizer_feature.py
+++ mindspeed/features_manager/optimizer/swap_optimizer_feature.py
@@ -4,23 +4,50 @@ from mindspeed.features_manager.feature import MindSpeedFeature
 
 
 class SwapOptimizerFeature(MindSpeedFeature):
-
     def __init__(self):
         super().__init__('swap-optimizer')
 
     def register_args(self, parser: ArgumentParser):
         group = parser.add_argument_group(title=self.feature_name)
         group.add_argument('--swap-optimizer', action='store_true', help='swap optimizer to cpu')
-        group.add_argument('--swap-optimizer-times', type=int, default=16,
-                           help='Each swap will be moved (len(shard_fp32_from_float16) // swap_optimizer_times) elements')
+        group.add_argument(
+            '--swap-optimizer-times',
+            type=int,
+            default=16,
+            help='Each swap will be moved (len(shard_fp32_from_float16) // swap_optimizer_times) elements',
+        )
 
     def validate_args(self, args):
         self.incompatible_check(args, 'reuse_fp32_param')
-        self.dependency_check(args, 'use_distributed_optimizer')
+        if (
+            getattr(args, self.feature_name, None)
+            and not getattr(args, "use_distributed_optimizer", None)
+            and not getattr(args, "use_layer_wise_distributed_optimizer", None)
+        ):
+            raise ValueError("Only support swap-optimizer when using distributed optimizer")
 
     def register_patches(self, patch_manager, args):
         if args.swap_optimizer:
-            from mindspeed.core.optimizer.swap_optimizer.swap_optimizer import SwapDistributedOptimizer, swap_adamw_step
-            patch_manager.register_patch('megatron.core.optimizer.distrib_optimizer.DistributedOptimizer',
-                                         SwapDistributedOptimizer)
-            patch_manager.register_patch('mindspeed.core.optimizer.adamw.AdamW.step', swap_adamw_step)
+            if 'adam' in args.optimizer:
+                from mindspeed.core.optimizer.swap_optimizer.swap_optimizer import (
+                    SwapDistributedOptimizer,
+                    swap_adamw_step,
+                )
+
+                patch_manager.register_patch(
+                    'megatron.core.optimizer.distrib_optimizer.DistributedOptimizer', SwapDistributedOptimizer
+                )
+                patch_manager.register_patch('mindspeed.core.optimizer.adamw.AdamW.step', swap_adamw_step)
+            elif 'muon' in args.optimizer:
+                from mindspeed.core.optimizer.swap_muon.swap_muon import (
+                    swap_layer_wise_distributed_optimizer_init_wrapper,
+                    swap_muon_step,
+                )
+
+                patch_manager.register_patch(
+                    'mindspeed.core.optimizer.muon.layer_wise_optimizer.LayerWiseDistributedOptimizer.__init__',
+                    swap_layer_wise_distributed_optimizer_init_wrapper,
+                )
+                patch_manager.register_patch(
+                    'mindspeed.core.optimizer.muon.emerging_optimizers.TensorParallelMuon.step', swap_muon_step
+                )
