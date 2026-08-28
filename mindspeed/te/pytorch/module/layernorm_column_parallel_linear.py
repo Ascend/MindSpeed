fix: fp16 TE rmsnorm overflow
Co-authored-by: yulelanmei<huangyijie8@huawei.com>



# message auto-generated for no-merge-commit merge:
!3914 merge 26.1.0_core_r0.12.1 into 26.1.0_core_r0.12.1

fix: fp16 TE rmsnorm overflow

Created-by: yulelanmei
Commit-by: yulelanmei
Merged-by: ascend-robot
Description: <span style="font-size:20px;">**What this PR does / why we need it?**</span>
RMSNorm in TE branch implemented through Python will easily overflow while using fp16 since it didn't set fp32 acumulation.

<span style="font-size:20px;">**Does this PR introduce any user-facing change?**</span>
N/A

<span style="font-size:20px;">**How was this patch tested?**</span>
Run testcase with --normalized RMSNorm.



See merge request: Ascend/MindSpeed!3914

mindspeed/te/pytorch/module/layernorm_column_parallel_linear.py
@@ -215,9 +215,11 @@ class MindSpeedTELayerNormColumnParallelLinear(torch.nn.Module):
     def _rmsnorm(self, inp):
         if self.config.use_fused_rmsnorm:
             return torch_npu.npu_rms_norm(inp, self.layer_norm_weight, epsilon=self.config.layernorm_epsilon)[0]
-        return (
-            inp * torch.rsqrt(inp.pow(2).mean(-1, keepdim=True) + self.config.layernorm_epsilon)
-        ) * self.layer_norm_weight
+        inp_dtype = inp.dtype
+        inp = inp.to(torch.float32)
+        var = inp.pow(2).mean(-1, keepdim=True)
+        inp = inp * torch.rsqrt(var + self.config.layernorm_epsilon)
+        return inp.to(inp_dtype) * self.layer_norm_weight
 
     def enable_recompute_norm(self, norm_ckpt):
         self.is_recompute_norm = True